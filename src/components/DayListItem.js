c7568b8a8e5145e8e8170ff11d154af441e65551
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-02-22 13:10:57 -0800

    Add formatSpots function to pass DayListItem tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,35 @@
 import React, { useState } from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 
 export default function DayListItem(props) {
+  
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`;
+    }
+  }
+  
+  let classType = "";
+  if (props.selected) {
+    classType = "--selected";
+  }
+  if (props.spots === 0) {
+    classType = "--full";
+  }
+  const dayClass = classNames(`day-list__item${classType}`);
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
