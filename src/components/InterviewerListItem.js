1dca216cc92c5ec397d7bae680193f8ccd70347b
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-02-23 00:14:17 -0800

    Refactor Application, List, and ListItem to use value/onChange

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,13 +9,13 @@ export default function InterviewerListItem(props) {
   })
 
   return (
-    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClass}>
+    <li onClick={props.setInterviewer} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected ? props.name : ""}
+      {props.selected && props.name}
     </li>
   );
 }
