351e8ae228c66ed1d304a1e363f953363fac38f8
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-02-24 01:14:51 -0800

    Add Appointment views to Application.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
 export default function Appointment(props) {
+  console.log(props);
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}></Header>
+      {props.interview ?
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer.name}
+      /> :
+      <Empty/>}
+    </article>
   );
 }
